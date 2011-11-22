1e28772acfcf728f1d9eb9a99be66eebfbceea42
msimon <msimon@physnet.uni-hamburg.de>
2011-11-22 15:31:57 +0100

    test_gauss.m gives a momentum.

--- test_gauss.m
+++ test_gauss.m
@@ -1,11 +1,20 @@
-u = (1:500)';
+n = 500;
+u = (1:n)';
 v = zeros(500,1);
 %v(350:450) = 0.004;
 
 [e,E] = get_hamiltonian_eigenvectors(v, 3);
 
-d = fit_distribution(E, normpdf(u, 200, 50));
 
-evolve_eigenvectors(e,E,0.01);
+k = 1;
+
+d_orig = normpdf(u, 200, 50);
+for j = 1:n
+    d_orig(j) = exp(1i*k*j) * d_orig(j);
+end
+
+d = fit_distribution(E, d_orig);
+
+%evolve_eigenvectors(e,E,0.01);
 
 plot_distr(E, d, v);
