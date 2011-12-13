d4872a893fb38b1c7fa3f96b2fd6e432d02d3e59
msimon <msimon@physnet.uni-hamburg.de>
2011-12-13 16:24:22 +0100

    Add test_deltapeak.m.

--- test_deltapeak.m
+++ test_deltapeak.m
@@ -5,11 +5,12 @@ v = zeros(n,1);
 %v(350:450) = 0.004;
 
 L = 1e-5;
+v(floor(n/2)) = n/L;
 
 [e,E] = get_hamiltonian_eigenvectors(v, L);
 
 
-k = 1e8 * pi;
+k = 20e6 * pi;
 
 d_orig = sqrt(normpdf(u, 0.1*n, 0.03*n));
 for j = 1:n
@@ -22,5 +23,5 @@ d = fit_distribution(E, d_orig, L);
 %disp('This should be 1:');
 %disp(sum(abs(E*d).^2));
 
-plot_slider(e, E, d, 1e-8*0.001*v/(norm(v,inf) * norm(d,inf)^2), 2e-9);
+plot_slider(e, E, d, 1e-8*0.001*v/(norm(v,inf) * norm(d,inf)^2), 6e-9);
 %plot_probabilities(E,e,1);
