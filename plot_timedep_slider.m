40674065da4341876a7a4af37376960d5ca2bb46
msimon <msimon@physnet.uni-hamburg.de>
2012-01-10 18:24:14 +0100

    Implements k space plotting in plot_timedep_slider.m.

--- plot_timedep_slider.m
+++ plot_timedep_slider.m
@@ -1,6 +1,7 @@
 function plot_timedep_slider(psi, n_t, dt, v_mat)
-    fig = figure();
-    slider = uicontrol(fig, 'Style', 'Slider', 'Units', 'normalized',...
+    x_plot = figure();
+    k_plot = figure();
+    slider = uicontrol(x_plot, 'Style', 'Slider', 'Units', 'normalized',...
         'Position', [0,-0.95,1,1]);
     warning('off','MATLAB:addlistener:invalidEventName');
     addlistener(slider,'ActionEvent',@(~,~) slider_moved());
@@ -9,6 +10,9 @@ function plot_timedep_slider(psi, n_t, dt, v_mat)
     function slider_moved()
         value = floor(get(slider, 'Value')*(n_t-1) + 1);
 
+        set(0,'CurrentFigure',x_plot);
         plot_timedep(psi(:,value), v_mat(:,value));
+        set(0,'CurrentFigure',k_plot);
+        plot_timedep(fft(psi(:,value)), 0*v_mat(:,1));
     end
 end
