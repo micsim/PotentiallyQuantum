ba09764c3fff2d6b4ef79e530ef16de13c0fda9a
Michael Simon <michael.the.simon@googlemail.com>
2012-01-17 17:06:19 +0100

    Optimizes create_gauss_distr.

--- create_gauss_distr.m
+++ create_gauss_distr.m
@@ -2,10 +2,12 @@
 function distribution = create_gauss_distr(n, L, mu, sigma, k)
     distribution = sqrt(normpdf((1:n)', mu*n, sigma*n));
     if k ~= 0
-        for j = 1:n
-            pos = j/n * L;
-            distribution(j) = exp(1i*k*pos) * distribution(j);
-        end
+        %for j = 1:n
+        %    pos = j/n * L;
+        %    distribution(j) = exp(1i*k*pos) * distribution(j);
+        %end
+        distribution(1:n) = exp(1i*k*L*((1:n)'/n)) .* distribution(1:n);
+        % Does the same as the above.
     end
     distribution = distribution/norm(distribution);
 end
