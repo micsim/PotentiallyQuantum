d0bf559109155b7b1af7e52f8ac4cbad6689d570
Michael Simon <michael.the.simon@googlemail.com>
2012-01-20 13:26:44 +0100

    Fix bug from commit 130e8e8.

--- cn_evolution.m
+++ cn_evolution.m
@@ -1,4 +1,4 @@
-function psi = cn_evolution(v_mat, psi_0, L, dt, kin_h_function, varargin)
+function psi = cn_evolution(v_mat, psi_0, L, dt, kin_h_function, mass)
     joule_to_eV = 6.24150974e18; % [eV/J]
     h_bar = 1.054571506e-34; % [J*s]
     h_bar = h_bar * joule_to_eV; % covert to [eV*s]
@@ -12,20 +12,23 @@ function psi = cn_evolution(v_mat, psi_0, L, dt, kin_h_function, varargin)
     assert(size(psi_0, 1) == n, 'psi_0 must have the same dimension as the potential in one point in time.');
     assert(size(psi_0, 2) == 1, 'psi_0 must be a column vector!');
 
-    if nargin < 5
-        kin_h_function = @get_kinetic_hamiltonian;
+    if nargin < 6
+        mass = 1;
+        if nargin < 5
+            kin_h_function = @get_kinetic_hamiltonian;
+        end
     end
 
 
     psi = zeros(n, n_t);
     psi(:,1) = psi_0;
 
-    T = kin_h_function(n,L,varargin{:});
+    T = kin_h_function(n,L,mass);
 
     for i = 1:n_t-1
         H = diag(v_mat(:,i)) + T;
         H2 = 1i/h_bar * H * dt / 2;
-    
+
         psi(:, i+1) = ((eye(n) + H2)\(eye(n) - H2)) * psi(:,i);
         % DOES NOT WORK: TODO why?
         %tmp = (eye(n) + 2*H2) * psi(:,i);
