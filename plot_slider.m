4a94855949bcd04695cb28d3f4a933d9887160e3
Michael Simon <michael.the.simon@googlemail.com>
2012-01-31 14:03:01 +0100

    Fix event name warnings by checking for the Matlab version.

--- plot_slider.m
+++ plot_slider.m
@@ -3,7 +3,14 @@ function plot_slider(e, E, d, v, t_max, K)
     slider = uicontrol(x_plot, 'Style', 'Slider', 'Units', 'normalized',...
         'Position', [0,-0.95,1,1]);
     warning('off','MATLAB:addlistener:invalidEventName');
-    addlistener(slider,'ActionEvent',@(~,~) slider_moved());
+    
+    if verLessThan('matlab', '7.13.0')
+        event_name = 'ActionEvent';
+    else
+        event_name = 'ContinuousValueChange';
+    end
+    % Different Matlab versions use different event names.
+    addlistener(slider, event_name,@(~,~) slider_moved());
 
     plot_distr(E, d, v);
 
--- plot_timedep_slider.m
+++ plot_timedep_slider.m
@@ -4,7 +4,14 @@ function plot_timedep_slider(psi, n_t, dt, v_mat)
     slider = uicontrol(x_plot, 'Style', 'Slider', 'Units', 'normalized',...
         'Position', [0,-0.95,1,1]);
     warning('off','MATLAB:addlistener:invalidEventName');
-    addlistener(slider,'ActionEvent',@(~,~) slider_moved());
+    
+    if verLessThan('matlab', '7.13.0')
+        event_name = 'ActionEvent';
+    else
+        event_name = 'ContinuousValueChange';
+    end
+    % Different Matlab versions use different event names.
+    addlistener(slider,event_name,@(~,~) slider_moved());
     slider_moved();
 
     function slider_moved()
